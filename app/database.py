ca6622c25c971df5da01da616af7e0be29bca422
Igor Barreto <igorbarreto1993@hotmail.com>
2022-10-14 02:15:43 -0300

    add sqlAlchemy configuration and remove data_seed

---
+++ app/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+USERNAME = "postgres"
+PASSWORD = "postgres"
+HOST = "localhost"
+DATABASE_NAME = "FastAPI"
+SQL_ALCHEMY_DATABASE_URL = f"postgresql://{USERNAME}:{PASSWORD}@{HOST}/{DATABASE_NAME}"
+
+engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
