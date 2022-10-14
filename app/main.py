034609c1ad89b9dc09367af100d32efc9e69b488
Igor Barreto <igorbarreto1993@hotmail.com>
2022-10-14 04:15:40 -0300

    create a Post model to create, update posts and create a post schema to visualization

--- app/main.py
+++ app/main.py
@@ -1,85 +1,76 @@
 from random import randrange
-from fastapi import FastAPI, status, HTTPException, Response
-from PostDTO import PostDTO
+from fastapi import FastAPI, status, HTTPException, Response, Depends
+from sqlalchemy.orm import Session
+from typing import List
 
-from data_seed import posts_seed
 
+from . import models
+from .schemas import Post as PostSchema, PostCreate
+from .models import Post as PostModel
+from .database import engine, get_db
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
-def find_post(id):
-    for post in posts_seed:
-        if post["id"] == id:
-            return post
-    return None
-
-
-def find_index(id):
-    for i, p in enumerate(posts_seed):
-        if p["id"] == id:
-            return i
-
-
 @app.get("/")
 async def root():
-    return {"message": "Bem vindo a minha API!"}
+    return {"message": "Welcome to my API"}
 
 
-@app.get("/posts")
-async def get_posts():
-    return {"data": posts_seed, "message": "Post found"}
+@app.get("/posts", response_model=List[PostSchema])
+async def get_posts(db: Session = Depends(get_db)):
+    posts = db.query(PostModel).all()
+    return posts
 
 
-@app.post("/create-post", status_code=status.HTTP_201_CREATED)
-async def create_post(new_post: PostDTO):
-    post_dict = new_post.dict()
-    post_dict["id"] = randrange(0, 1000000)
-    posts_seed.append(post_dict)
-    return {"data": post_dict, "message": "Post created"}
+@app.post(
+    "/create-post", status_code=status.HTTP_201_CREATED, response_model=PostSchema
+)
+async def create_post(post: PostCreate, db: Session = Depends(get_db)):
+    new_post = PostModel(**post.dict())
+    post.dict()
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return new_post
 
 
-@app.get("/posts/latest")
-def get_latest_post():
-    post = posts_seed[len(posts_seed) - 1]
-    return {"data": post, "message": "Post found"}
-
-
-@app.get("/posts/{id}")
-def get_post(id: int):
-    post = find_post(id)
+@app.get("/posts/{id}", response_model=PostSchema)
+def get_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(PostModel).filter(PostModel.id == id).first()
     if not post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Post with {id} was not found",
+            detail="Post not found",
         )
-    return {"data": post, "message": "Post found"}
+    return post
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_posst(id: int):
-    index = find_index(id)
-    if not index:
+def delete_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(PostModel).filter(PostModel.id == id)
+    print(post)
+    if not post.first():
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Post with{id} does not exists",
+            detail=f"Post does not exists",
         )
-    posts_seed.pop(index)
+    post.delete()
+    db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@app.put("/posts/{id}")
-def update_post(id: int, post: PostDTO):
-    index = find_index(id)
-    if not index:
+@app.put("/posts/{id}", response_model=PostSchema)
+def update_post(id: int, updated_post: PostCreate, db: Session = Depends(get_db)):
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+    if not post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Post with{id} does not exists",
+            detail=f"Post does not exists",
         )
-    post_dict = post.dict()
-    post_dict["id"] = id
-    posts_seed[index] = post_dict
-    return {
-        "data": post_dict,
-        "message": "Updated post",
-    }
+    post_query.update(updated_post.dict(), synchronize_session=False)
+    db.commit()
+    return post_query.first()
---
+++ app/models.py
@@ -0,0 +1,16 @@
+from typing import Optional
+from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP
+from sqlalchemy.sql.expression import text
+from .database import Base
+
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    published = Column(Boolean, server_default="True", nullable=False)
+    created_at = Column(
+        TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False
+    )
