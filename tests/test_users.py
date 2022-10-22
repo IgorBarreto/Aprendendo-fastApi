32be0100ea4d22eb636af2f851e02a4557fc51e5
Igor Barreto <igorbarreto1993@hotmail.com>
2022-10-22 03:10:00 -0300

    create a github actions

--- tests/test_users.py
+++ tests/test_users.py
@@ -3,6 +3,7 @@ from fastapi import status, HTTPException
 
 from pytest import raises
 from app.main import app
+from app.schemas import User
 
 client = TestClient(app)
 
@@ -21,9 +22,10 @@ def test_create_user():
     res = client.post(
         "/users/", json={"email": "igorteste@gmail.com", "password": "senha123"}
     )
+    new_user = User(**res.json())
     id = res.json().get("id")
     assert res.status_code == status.HTTP_201_CREATED
-
+    assert new_user.email == "igorteste@gmail.com"
     client.delete(f"/users/{id}")
 
 
@@ -38,3 +40,9 @@ def test_email_alread_exists():
     assert res.status_code == status.HTTP_409_CONFLICT
     assert res.json().get("detail") == "Email already exists"
     client.delete(f"/users/{id}")
+
+
+def test_user_login():
+    res = client.post("/login", data={"username": "igor@gmail.com", "email": "pass"})
+
+    assert res.status_code == status.HTTP_200_OK
